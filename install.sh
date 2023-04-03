1c1c2cf537638a9536c0567b7f7afec839fab6e6
ahmadsailpil <106122054+ahmadsailpil@users.noreply.github.com>
2023-04-03 17:26:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ set -e
 
 export GITHUB_SOURCE="v0.12.1"
 export SCRIPT_RELEASE="v0.12.1"
-export GITHUB_BASE_URL="https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer"
+export GITHUB_BASE_URL="https://raw.githubusercontent.com/ahmadsailpil/pterodactyl-installer"
 
 LOG_PATH="/var/log/pterodactyl-installer.log"
 
